3ad982a8dc4bc710ee6be7521b810f3a74cf8c2a
Wanjin Maglangit <wmaglangit1118@gmail.com>
2025-07-01 19:29:10 +0800

    done on delete, starting on search

--- JavaContactBook.java
+++ JavaContactBook.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 public class JavaContactBook {
 
     // createContact(String name)	Adds a new Contact
@@ -20,8 +22,35 @@ public class JavaContactBook {
         Contact newContact = new Contact(name);
         contacts.add(newContact);
     }
+
     // deleteContact(String name)	Removes by name (or ID)
+    public boolean deleteContact(List<Contact> contacts, String name){
+        Iterator<Contact> iterator = contacts.iterator();
+        boolean removed = false;
+        while(iterator.hasNext()){
+            Contact contact=iterator.next();
+            if(contact.getName().equalsIgnoreCase(name)){
+                iterator.remove();
+                removed = true;
+                System.out.println("Contact Removed");
+                break;
+            }
+        }
+        if(!removed){
+            System.out.println("Contact not found");
+        }
+        return removed;
+    }
+    
     // searchContact(String query)
+    public boolean searchContact(List<Contact> contacts, String query){
+        for(Contact contact : contacts){
+            if(contact.getName().equalsIgnoreCase(query)){
+                contact.printContact();
+            }
+        }
+        return true
+    }
 
 
     public static void main(String[] args) {
